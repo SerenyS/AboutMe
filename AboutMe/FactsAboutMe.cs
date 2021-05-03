c1726f864741398dd336f2e1f05448a8ee7258e8
Sereny Sanchez <52845840+SerenyS@users.noreply.github.com>
2021-05-03 15:01:01 -0500

    Add project files.

---
+++ AboutMe/FactsAboutMe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AboutMe
+{
+    class FactsAboutMe
+    {
+        public FactsAboutMe() { }
+
+        public static IEnumerable<FactsAboutMe> All { private set; get; }
+        public string TheFact { get; set; }
+        public string ShortFact { get; set; }
+
+        static FactsAboutMe()
+        {
+            List<FactsAboutMe> all = new List<FactsAboutMe>();
+            all.Add(new FactsAboutMe() { TheFact = "I grew up in a small town in Jalisco Mexico", ShortFact = "Where I grew up" });
+            all.Add(new FactsAboutMe() { TheFact = "Olive green!", ShortFact = "My favorite color" });
+            all.Add(new FactsAboutMe() { TheFact = "23 years young", ShortFact = "My age" });
+            all.Add(new FactsAboutMe() { TheFact = "Photography and digital art", ShortFact = "My hobbies" });
+            all.Add(new FactsAboutMe() { TheFact = "Thai food and cheesecake", ShortFact = "My favorite food" });
+            All = all;
+        }
+    }
+}
